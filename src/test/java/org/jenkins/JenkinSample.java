149211ed29ac5619040f5a15fc0c3e767ff8d2af
AswinKumar17 <aswinkumar1710@gmail.com>
2022-05-18 14:58:30 +0530

    gitone

---
+++ src/test/java/org/jenkins/JenkinSample.java
@@ -0,0 +1,25 @@
+package org.jenkins;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class JenkinSample {
+	@Test
+ public void test(){
+System.out.println("Test 5 Pass");
+}
+	@Test
+ public void test9(){
+System.out.println("Test 9 Pass");
+}
+	@Test
+ public void test3(){
+System.out.println("Test 3 Pass");
+}
+	@Ignore
+	 public void w(){
+	System.out.println("Test w Pass");
+	}
+
+
+}
